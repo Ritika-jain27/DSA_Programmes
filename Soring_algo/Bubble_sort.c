8722a407f65d26145faeaea26f5a4d13d3b89da6
Ritika-jain27 <ritikaj088@gmail.com>
2022-01-30 13:09:04 +0530

    Update Bubble_sort.c

--- Soring_algo/Bubble_sort.c
+++ Soring_algo/Bubble_sort.c
@@ -1,42 +1,41 @@
 #include <stdio.h>
 
-int main(){
-	int n,num,i,j,temp,flag;
+int main()
+{
+	int n, num, i, j, temp, flag;
 	printf("Enter the size of array");
-	scanf("%d",&n);
-    int arr[n];
-    
-    for(i=0;i<n;i++)
-    {
-    	printf("Enter %d element of array :\n",i+1);
-    	scanf("%d",&num);
-    	arr[i]=num;
-	}
-	
-	for(i=0;i<n-1;i++)
+	scanf("%d", &n);
+	int arr[n];
+
+	for (i = 0; i < n; i++)
 	{
-		flag=0;
-		for(j=1;j<n-i;j++)
+		printf("Enter %d element of array :\n", i + 1);
+		scanf("%d", &num);
+		arr[i] = num;
+	}
+
+	for (i = 0; i < n - 1; i++)
+	{
+		flag = 0;
+		for (j = 1; j < n - i; j++)
 		{
-			if(arr[j-1]>arr[j])
+			if (arr[j - 1] > arr[j])
 			{
-			temp=arr[j-1];
-				arr[j-1]=arr[j];
-				arr[j]=temp;
-				flag=1;
+				temp = arr[j - 1];
+				arr[j - 1] = arr[j];
+				arr[j] = temp;
+				flag = 1;
 			}
 		}
-		if(flag==0)
+		if (flag == 0)
 		{
 			break;
 		}
-		
 	}
-	
+
 	printf(" Sorted array:\n");
-	for(i=0;i<n;i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d ",arr[i]);
+		printf("%d ", arr[i]);
 	}
-	
 }
